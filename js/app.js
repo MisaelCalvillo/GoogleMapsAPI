5fff646bc315f1a4bb16fb3803999db4fe06340d
Misael Calvillo Mancilla <calvillomisa@hotmail.com>
2018-07-09 19:56:55 -0500

    Feat: Agrega varios markers al mapa

--- js/app.js
+++ js/app.js
@@ -1,47 +1,27 @@
-// Inicializa e inserta el Mapa
+// Inicializa e inserta el Mapa en una variable global que se pueda compartir entre funciones
+var map;
+
 function initMap() {
+  map = new google.maps.Map( document.getElementById('map'), { 
+        zoom: 2, 
+        center: new google.maps.LatLng(2.8, -187.3),
+        mapTypeId: 'terrain'
+   });
 
-  var map = new google.maps.Map( document.getElementById('map'), { zoom: 3, center: {lat: -28.024, lng: 140.887} } );
+   var script = document.createElement('script');
 
-  // Lugar en donde estaba el marcador
-  // var marker = new google.maps.Marker({ position:  {lat: -28.024, lng: 140.887} , map: map });
+   script.src = 'https://developers.google.com/maps/documentation/javascript/examples/json/earthquake_GeoJSONP.js';
+   document.getElementsByTagName('head')[0].appendChild(script);
+  }
+   // Itera a travez de array de resultados y coloca un marcador por cada set de coordenadas
+   window.eqfeed_callback = function(results) {
+     for(var i = 0; i < results.features.length; i++) {
+       var coords = results.features[i].geometry.coordinates;
+       var latLng = new google.maps.LatLng(coords[1], coords[0]);
+       var marker = new google.maps.Marker({
+         position: latLng,
+         map: map
+       });
+     }
+   }
 
-  var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-  var locations = [
-    {lat: -31.563910, lng: 147.154312},
-    {lat: -33.718234, lng: 150.363181},
-    {lat: -33.727111, lng: 150.371124},
-    {lat: -33.848588, lng: 151.209834},
-    {lat: -33.851702, lng: 151.216968},
-    {lat: -34.671264, lng: 150.863657},
-    {lat: -35.304724, lng: 148.662905},
-    {lat: -36.817685, lng: 175.699196},
-    {lat: -36.828611, lng: 175.790222},
-    {lat: -37.750000, lng: 145.116667},
-    {lat: -37.759859, lng: 145.128708},
-    {lat: -37.765015, lng: 145.133858},
-    {lat: -37.770104, lng: 145.143299},
-    {lat: -37.773700, lng: 145.145187},
-    {lat: -37.774785, lng: 145.137978},
-    {lat: -37.819616, lng: 144.968119},
-    {lat: -38.330766, lng: 144.695692},
-    {lat: -39.927193, lng: 175.053218},
-    {lat: -41.330162, lng: 174.865694},
-    {lat: -42.734358, lng: 147.439506},
-    {lat: -42.734358, lng: 147.501315},
-    {lat: -42.735258, lng: 147.438000},
-    {lat: -43.999792, lng: 170.463352}
-  ]
-
-  var markers = locations.map(function(location, i) {
-    return new google.maps.Marker({
-      position: location,
-      label: labels[i % labels.length]
-    });
-  });
-
-  var markerCluster = new MarkerClusterer(map, markers, 
-   {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
-
-
-}
